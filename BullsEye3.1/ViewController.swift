cef2857c447234ae00d2ea947cd4df6d6ecf9a85
Joe Lucero <jlucerond@gmail.com>
2016-08-23 11:08:50 -0400

    commit to master
    added all buttons and basic play to game

--- BullsEye3.1/ViewController.swift
+++ BullsEye3.1/ViewController.swift
@@ -10,22 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var currentValue: Int = 50
-
+    // variables
+    var currentValue: Int = 0
+    var targetValue: Int = 0
+    
+    // outlets
+    @IBOutlet weak var slider: UISlider!
+    
+    //functions
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        currentValue = lroundf(slider.value)
+        targetValue = Int(arc4random_uniform(100)) + 1
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    //IBActions
     @IBAction func buttonPressed () {
         
-        let alert = UIAlertController (title: "top line",
-                                       message: "you got: \(currentValue)",
+        let message = "The target was: \(targetValue)" + "\nThe value of the slider is: \(currentValue)"
+        
+        let alert = UIAlertController (title: "Hello, World",
+                                       message: message,
                                        preferredStyle: .Alert)
         
         let action = UIAlertAction (title: "click here",
@@ -37,7 +48,6 @@ class ViewController: UIViewController {
         presentViewController(alert, animated: true, completion: nil)
         
     }
-
     @IBAction func sliderMoved(sender: UISlider) {
         
         currentValue = lroundf(sender.value)
